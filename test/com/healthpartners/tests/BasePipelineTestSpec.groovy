6c27d64bde637ddb3608cb36cfb8ae11fd6926e2
jmcshane <jmcshan1@gmail.com>
2018-05-16 21:21:45 -0500

    Set environment variable

--- test/com/healthpartners/tests/BasePipelineTestSpec.groovy
+++ test/com/healthpartners/tests/BasePipelineTestSpec.groovy
@@ -4,11 +4,12 @@ class BasePipelineTestSpec extends BasePipelineTest {
     def 'base pipeline test initalizes mocks and executes script'() {
         given:
         def binding = initializeMocks('testMock')
+        setEnv(binding, [ENV_VAR: 'value'])
         def testScript = getShell(binding, 'test/testScript.groovy')
         when:
         def output = testScript()
         then:
         1*binding.testMock.call() >> "output"
-        output == "output"
+        output == "output/value"
     }
 }
--- test/testScript.groovy
+++ test/testScript.groovy
@@ -1,5 +1,5 @@
 def call() {
-    testMock()
+    "${testMock()}/${env.ENV_VAR}"
 }
 
 return this
